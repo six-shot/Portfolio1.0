e43ffe250fc48451c40989dafbd404874bb31d41
Your Name <okhuomonajayi54@gmail.com>
2024-01-10 00:42:48 +0100

    fix

--- src/components/header/burger/index.jsx
+++ src/components/header/burger/index.jsx
@@ -4,13 +4,33 @@ import styles from './style.module.scss';
 export default function Burger({openMenu}) {
 
     return (
-        <div onClick={() => {openMenu()}} className={styles.button}>
-            <div className={styles.background}></div>
-            <svg width="56" height="7" viewBox="0 0 56 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <line x1="56" y1="0.5" x2="4.37114e-08" y2="0.500005" stroke="white"/>
-                <line x1="56" y1="6.5" x2="28" y2="6.5" stroke="white"/>
-            </svg>
-            <p>Menu</p>
-        </div>
-    )
+      <div
+        onClick={() => {
+          openMenu();
+        }}
+        className="w-[150px] h-[80px] bg-black hover:bg-[#76d5cb] hover:text-black text-white transition duration-500 flex flex-col justify-between fixed right-0 top-0 p-[10px] cursor-pointer"
+      >
+        <div className="bg-[#76d5cb] w-full absolute left-0 top-0 h-0 -z-[1] transition duration-300 hover:h-full"></div>
+        <svg
+          width="56"
+          height="7"
+          viewBox="0 0 56 7"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <line
+            x1="56"
+            y1="0.5"
+            x2="4.37114e-08"
+            y2="0.500005"
+            stroke="white"
+          />
+          <line x1="56" y1="6.5" x2="28" y2="6.5" stroke="white" />
+        </svg>
+        <p className=" uppercase  ">
+          Menu
+        </p>
+      </div>
+    );
 }
+
