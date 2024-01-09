573d93651af1c9368c35d1b45b1e335ca4270a35
Your Name <okhuomonajayi54@gmail.com>
2024-01-09 17:39:30 +0100

    fix

--- src/components/header/menu/index.jsx
+++ src/components/header/menu/index.jsx
@@ -3,6 +3,8 @@ import { opacity, slideLeft, mountAnim } from '../anim';
 import styles from './style.module.scss';
 import Link from './link';
 import { useState } from 'react';
+import { IoCloseOutline } from "react-icons/io5";
+import { IconContext } from 'react-icons';
 
 const menu = [
   {
@@ -26,42 +28,44 @@ const menu = [
 export default function index({closeMenu}) {
 
   return (
-    <motion.div className={styles.menu} variants={opacity} initial="initial" animate="enter" exit="exit">
-
-        <div className={styles.header}>
-          <motion.svg 
-            variants={slideLeft} 
-            {...mountAnim}
-            onClick={() => {closeMenu()}} 
-            width="68" 
-            height="68" 
-            viewBox="0 0 68 68" 
-            fill="none" 
-            xmlns="http://www.w3.org/2000/svg">
-              <path d="M1.5 1.5L67 67" stroke="white"/>
-              <path d="M66.5 1L0.999997 66.5" stroke="white"/>
-          </motion.svg>
-        </div>
-
-        <div className={styles.body}>
-          {
-            menu.map( (el, index) => {
-              return <Link data={el} index={index} key={index}/>
-            })
-          }
-        </div>
-
-        <motion.div 
-          variants={opacity} 
-          {...mountAnim} 
-          custom={0.5} 
-          className={styles.footer}>
-          <a>GT</a>
-          <a>X</a>
-          <a>IG</a>
-          <a>IN</a>
+    <motion.div
+      className={styles.menu}
+      variants={opacity}
+      initial="initial"
+      animate="enter"
+      exit="exit"
+    >
+      <div className={styles.header}>
+        <motion.div
+          variants={slideLeft}
+          {...mountAnim}
+          onClick={() => {
+            closeMenu();
+          }}
+        >
+          <IconContext.Provider value={{color:"white"}}>
+            <IoCloseOutline />
+          </IconContext.Provider>
         </motion.div>
+      </div>
 
+      <div className={styles.body}>
+        {menu.map((el, index) => {
+          return <Link data={el} index={index} key={index} />;
+        })}
+      </div>
+
+      <motion.div
+        variants={opacity}
+        {...mountAnim}
+        custom={0.5}
+        className={styles.footer}
+      >
+        <a>GT</a>
+        <a>X</a>
+        <a>IG</a>
+        <a>IN</a>
+      </motion.div>
     </motion.div>
-  )
+  );
 }
