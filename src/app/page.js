93c4b0a7351d52a39ebe53c51a229e9f7e363a9a
Your Name <okhuomonajayi54@gmail.com>
2024-01-11 11:04:16 +0100

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import Hero from "@/components/Hero";
 import styles from "./page.module.css";
 import About from "@/components/about/About";
+import Galllery from "@/components/gallery";
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Hero />
       <About />
+      <Galllery/>
+      
     </main>
   );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Header from './header'
+import Line from './Line'
 
 export default function Hero() {
   return (
-    <div className='h-screen'>
+    <div className='h-screen  w-full '>
         <Header/>
         <div className=''>
         </div>
